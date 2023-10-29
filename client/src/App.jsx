aa9fab3b9d222ba6a7f0f57effdc55de61d23ee0
Vanshika <vanshika.agar@gmail.com>
2023-10-29 22:23:57 +0530

    add new place form and photo upload feature

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,30 +8,30 @@ import axios from "axios";
 import { UserContextProvider } from "./UserContext";
 import ProfilePage from "./pages/ProfilePage";
 import PlacesPage from "./pages/PlacesPage";
-import PlacesFormPage from "./pages/PlacesFormPage";
+import PlacesFormPage from "./pages/AddNewPlace/PlacesFormPage";
 import BookingsPage from "./pages/BookingsPage";
 import ViewPlacePage from "./pages/ViewPlacePage";
 import BookedPage from "./pages/BookedPage";
 
-axios.defaults.baseURL = "http://127.0.0.1:5000";
+axios.defaults.baseURL = "http://localhost:5000";
 axios.defaults.withCredentials = true;
 function App() {
   return (
     <UserContextProvider>
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<IndexPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/account" element={<ProfilePage />} />
-        <Route path="/account/places" element={<PlacesPage />} />
-        <Route path="/account/places/new" element={<PlacesFormPage />} />
-        <Route path="/account/places/:id" element={<PlacesFormPage />} />
-        <Route path="/place/:id" element={<ViewPlacePage />} />
-        <Route path="/account/bookings" element={<BookingsPage />} />
-        <Route path="/account/bookings/:id" element={<BookedPage />} />
-      </Route>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<IndexPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/account" element={<ProfilePage />} />
+          <Route path="/account/places" element={<PlacesPage />} />
+          <Route path="/account/places/new" element={<PlacesFormPage />} />
+          <Route path="/account/places/:id" element={<PlacesFormPage />} />
+          <Route path="/place/:id" element={<ViewPlacePage />} />
+          <Route path="/account/bookings" element={<BookingsPage />} />
+          <Route path="/account/bookings/:id" element={<BookedPage />} />
+        </Route>
+      </Routes>
     </UserContextProvider>
   );
 }
